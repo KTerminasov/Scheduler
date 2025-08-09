dcec700b150109364b03e06ea8546370a2c09f5d
Kirill Terminasov <kterminasov@gmail.com>
2025-08-09 09:05:09 +0300

    Add Scheduler class and unit tests for busy slots retrieval

---
+++ scheduler.py
@@ -0,0 +1,48 @@
+import requests
+
+
+class Scheduler:
+    """Класс, реализующий работу с занятостью работника."""
+
+    def __init__(self, url):
+        """Инициализация класса."""
+        self.url = url
+        self.data = self._get_data()
+        self.days = {day['date']: day for day in self.data['days']}
+        self.timeslots = self._parse_timeslots()
+
+    def _get_data(self):
+        """Получение данных с помощью GET-запроса."""
+        response = requests.get(self.url, timeout=10)
+        return response.json()
+
+    def _parse_timeslots(self):
+        """Парсинг временных слотов и привязка их к датам."""
+        timeslots = {}
+
+        for day in self.data['days']:
+            date = day['date']
+            timeslots[date] = []
+
+        for timeslot in self.data['timeslots']:
+            day_id = timeslot['day_id']
+            day = next(
+                (d for d in self.data['days'] if d['id'] == day_id),
+                None
+            )
+            if day:
+                date = day['date']
+                timeslots[date].append((timeslot['start'], timeslot['end']))
+
+        for date, slots in timeslots.items():
+            slots.sort(key=lambda x: x[0])
+
+        return timeslots
+
+    def get_busy_slots(self, date):
+        """Получение всех занятых промежутков для указанной даты."""
+
+        if date not in self.days or not self.timeslots[date]:
+            return []
+
+        return self.timeslots[date]
---
+++ test_scheduler.py
@@ -0,0 +1,20 @@
+import unittest
+from scheduler import Scheduler
+
+
+class TestScheduler(unittest.TestCase):
+    """Тестирование работы с занятостью работника."""
+
+    def setUp(self):
+        self.scheduler = Scheduler(
+            url="https://ofc-test-01.tspb.su/test-task/"
+        )
+
+    def test_get_busy_slots(self):
+        """Проверка получения занятых слотов."""
+
+        result = self.scheduler.get_busy_slots("2025-02-15")
+        self.assertEqual(result, [("09:00", "12:00"), ("17:30", "20:00")])
+
+        result = self.scheduler.get_busy_slots("2024-10-12")
+        self.assertEqual(result, [])
